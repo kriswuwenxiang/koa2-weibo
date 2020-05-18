455a4579e1cbb878196b6fd69b8b4d0081a71a35
kriswuwenxiang <794783332@qq.com>
2020-05-18 22:43:28 +0800

    feat: radis操作

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,4 +10,9 @@ router.get('/bar', function (ctx, next) {
   ctx.body = 'this is a users/bar response'
 })
 
+router.post('/login', function (ctx, next) {
+  const { username, password } = ctx.request.body
+  ctx.body = { username, password }
+})
+
 module.exports = router
