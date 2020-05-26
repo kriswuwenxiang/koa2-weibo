d10b47fdd01ab6649de11b5d0180806e7342e75c
kriswuwenxiang <794783332@qq.com>
2020-05-26 23:05:18 +0800

    feat: 用户信息校验功能

--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -13,16 +13,15 @@ const {
     changePassword,
     logout
 } = require('../../controller/user')
-// const userValidate = require('../../validator/user')
-// const { genValidator } = require('../../middlewares/validator')
+const userValidate = require('../../validator/user')
+const { genValidator } = require('../../middlewares/validator')
 const { isTest } = require('../../config/env')
 const { loginCheck } = require('../../middlewares/loginChecks')
 
 router.prefix('/api/user')
 
 // 注册路由
-// router.post('/register', genValidator(userValidate), async (ctx, next) => {
-router.post('/register', async (ctx, next) => {
+router.post('/register', genValidator(userValidate), async (ctx, next) => {
     const { userName, password, gender } = ctx.request.body
     ctx.body = await register({
         userName,
@@ -53,15 +52,13 @@ router.post('/delete', loginCheck, async (ctx, next) => {
 })
 
 // 修改个人信息
-// router.patch('/changeInfo', loginCheck, genValidator(userValidate), async (ctx, next) => {
-router.patch('/changeInfo', loginCheck, async (ctx, next) => {
+router.patch('/changeInfo', loginCheck, genValidator(userValidate), async (ctx, next) => {
     const { nickName, city, picture } = ctx.request.body
     ctx.body = await changeInfo(ctx, { nickName, city, picture })
 })
 
 // 修改密码
-// router.patch('/changePassword', loginCheck, genValidator(userValidate), async (ctx, next) => {
-router.patch('/changePassword', async (ctx, next) => {
+router.patch('/changePassword', loginCheck, genValidator(userValidate), async (ctx, next) => {
     const { password, newPassword } = ctx.request.body
     const { userName } = ctx.session.userInfo
     ctx.body = await changePassword(userName, password, newPassword)
